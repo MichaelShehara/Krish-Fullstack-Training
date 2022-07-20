a941d3a17e51298dbf8844632916606b6aa00ba3
MichaelShehara <sheharaluvis@gmail.com>
2022-07-20 21:19:34 +0530

    Created spring boot project

---
+++ sample-springboot-project/src/main/java/com/shehara/springboot/SampleSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shehara.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ sample-springboot-project/src/test/java/com/shehara/springboot/SampleSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shehara.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
